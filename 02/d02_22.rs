28469869bd707d378449b5a146509efeea16b86d
Joel Juhela <joel.juhela@protonmail.com>
2022-12-02 14:36:57 +0200

    Day 2

---
+++ 02/d02_22.rs
@@ -0,0 +1,66 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error};
+
+#[derive(PartialEq)]
+enum RPSChoice {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+fn round_win(l: RPSChoice, r: RPSChoice) -> i32 {
+    // 2: l wins, 1: tie, 0: r wins
+    if l == r {
+        return 1;
+    } else {
+        let result = match l {
+            RPSChoice::Rock if r == RPSChoice::Scissors => 2,
+            RPSChoice::Paper if r == RPSChoice::Rock => 2,
+            RPSChoice::Scissors if r == RPSChoice::Paper => 2,
+            _ => 0,
+        };
+        return result;
+    }
+}
+
+fn str_to_rps(c_str: &str) -> RPSChoice {
+    let choice = match c_str {
+        "A" | "X" => RPSChoice::Rock,
+        "B" | "Y" => RPSChoice::Paper,
+        "C" | "Z" => RPSChoice::Scissors,
+        &_ => panic!("Non-valid choice"),
+    };
+    return choice;
+}
+
+
+fn calc_match(me: &str, opp: &str) -> i32 {
+    let my_rps = str_to_rps(me);
+    let opp_rps = str_to_rps(opp);
+
+    let my_rps_value = match my_rps {
+        RPSChoice::Rock => 1,
+        RPSChoice::Paper => 2,
+        RPSChoice::Scissors => 3
+    };
+
+    let win_score = 3 * round_win(my_rps, opp_rps);
+    let result = my_rps_value + win_score;
+    return result;
+}
+
+fn main() -> Result<(), Error> {
+    let mut score = 0;
+
+    let file = File::open("./input")?;
+    let buff = BufReader::new(file);
+
+    for line in buff.lines() {
+        let row = line?;
+        let values: Vec<&str> = row.split(" ").collect();
+        score += calc_match(values[1], values[0]);
+    }
+
+    println!("final score: {}", score);
+    Ok(())
+}
---
+++ 02/d02_2_22.rs
@@ -0,0 +1,105 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader, Error};
+
+#[derive(PartialEq)]
+enum RPSChoice {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl RPSChoice {
+    fn value(&self) -> i32 {
+        let result = match &self {
+            RPSChoice::Rock => 1,
+            RPSChoice::Paper => 2,
+            RPSChoice::Scissors => 3
+        };
+        return result;
+    }
+}
+
+enum Outcome {
+    Win,
+    Tie,
+    Lose,
+}
+
+fn win(rps: &RPSChoice) -> i32 {
+    let result = match &rps {
+        RPSChoice::Rock => RPSChoice::Paper.value(),
+        RPSChoice::Paper => RPSChoice::Scissors.value(),
+        RPSChoice::Scissors => RPSChoice::Rock.value(),
+    };
+    return result;
+}
+
+fn lose(rps: &RPSChoice) -> i32 {
+    let result = match &rps {
+        RPSChoice::Rock => RPSChoice::Scissors.value(),
+        RPSChoice::Paper => RPSChoice::Rock.value(),
+        RPSChoice::Scissors => RPSChoice::Paper.value(),
+    };
+    return result;
+}
+
+fn calc_rps_value(opp_rps: &RPSChoice, outcome: &Outcome) -> i32 {
+    let result = match outcome {
+        Outcome::Lose => lose(opp_rps),
+        Outcome::Tie => opp_rps.value(),
+        Outcome::Win => win(opp_rps),
+    };
+    return result;
+}
+
+fn str_to_outcome(o_str: &str) -> Outcome {
+    let result = match o_str {
+        "X" => Outcome::Lose,
+        "Y" => Outcome::Tie,
+        "Z" => Outcome::Win,
+        &_ => panic!("Non-valid outcome"),
+    };
+    return result;
+}
+
+fn str_to_rps(c_str: &str) -> RPSChoice {
+    let choice = match c_str {
+        "A" => RPSChoice::Rock,
+        "B" => RPSChoice::Paper,
+        "C" => RPSChoice::Scissors,
+        &_ => panic!("Non-valid choice"),
+    };
+    return choice;
+}
+
+
+fn calc_match(me: &str, opp: &str) -> i32 {
+    let opp_rps = str_to_rps(opp);
+    let my_outcome = str_to_outcome(me);
+
+    let my_rps_value = calc_rps_value(&opp_rps, &my_outcome);
+    let win_score = match my_outcome {
+        Outcome::Win => 6,
+        Outcome::Tie => 3,
+        Outcome::Lose => 0,
+    };
+
+    let result = my_rps_value + win_score;
+    return result;
+}
+
+fn main() -> Result<(), Error> {
+    let mut score = 0;
+
+    let file = File::open("./input")?;
+    let buff = BufReader::new(file);
+
+    for line in buff.lines() {
+        let row = line?;
+        let values: Vec<&str> = row.split(" ").collect();
+        score += calc_match(values[1], values[0]);
+    }
+
+    println!("final score: {}", score);
+    Ok(())
+}
